8913902d218515843696db2d9b6bf0c5ee0951c3
Arjan Scherpenisse <arjan@scherpenisse.net>
2016-10-14 15:52:51 +0200

    Add mix hex package metadata

--- mix.exs
+++ mix.exs
@@ -3,11 +3,26 @@ defmodule CookieStore.Mixfile do
 
   def project do
     [app: :cookie_store,
+     name: "CookieStore",
      version: "0.1.0",
      elixir: "~> 1.3",
+     description: description(),
+     package: package(),
+     source_url: "https://github.com/arjan/cookie_store",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
+  end
+
+  defp description do
+    "Library for parsing of HTTP cookies and persistent storage"
+  end
+
+  defp package do
+    %{files: ["lib", "mix.exs", "*.md", "LICENSE"],
+      maintainers: ["Arjan Scherpenisse"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/arjan/cookie_store"}}
   end
 
   # Configuration for the OTP application
@@ -17,15 +32,6 @@ defmodule CookieStore.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
   end
